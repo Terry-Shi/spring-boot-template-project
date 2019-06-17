0a1eb0462e1e9aa9ecfc89784103ef6094187e4a
Terry-Shi <terryemailbox@gmail.com>
2019-06-17 21:00:22 +0800

    add timeout and retry for WebClient

--- src/main/java/com/terry/webapp/util/WebClientUtils.java
+++ src/main/java/com/terry/webapp/util/WebClientUtils.java
@@ -1,11 +1,19 @@
 package com.terry.webapp.util;
 
+import java.io.IOException;
+import java.time.Duration;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
+import reactor.retry.Backoff;
+import reactor.retry.Retry;
 
 @Service
 class WebClientUtils {
@@ -16,7 +24,10 @@ class WebClientUtils {
 		WebClient.Builder webClientBuilder = WebClient.builder();
 		WebClientUtils webClientUtils = new WebClientUtils(webClientBuilder);
 		//webClientUtils.testWithCookie();
-		webClientUtils.testPostJson();
+		//webClientUtils.testPostJson();
+		//webClientUtils.testPostJsonTimeout();
+		//webClientUtils.testPostJsonRetry();
+		webClientUtils.testFormParam();
 	}
 	
     public WebClientUtils(WebClient.Builder webClientBuilder) {
@@ -42,8 +53,66 @@ class WebClientUtils {
                 .uri("https://httpbin.org/anything")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .body(Mono.just(book), Book.class)
-                .retrieve().bodyToMono(String.class);
+                .retrieve()
+                .bodyToMono(String.class);
         System.out.println("result:{}" + resp.block());
+    }
+	
+	/**
+	 * Timeout
+	 * @throw java.util.concurrent.TimeoutException
+	 */
+	public void testPostJsonTimeout(){
+        Book book = new Book();
+        book.setName("name");
+        book.setTitle("this is title");
+        Mono<String> resp = webClient.post()
+                .uri("https://httpbin.org/delay/5")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(book), Book.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(3)); // <-- 本质上是处理response的环节发生timeout;
+        System.out.println("result:{}" + resp.block());
+    }
+	
+	public void testPostJsonRetry() {
+        Book book = new Book();
+        book.setName("name");
+        book.setTitle("this is title");
+        Retry<?> retry = Retry.any() //
+                .retryMax(2) //
+                .backoff(Backoff.fixed(Duration.ofMillis(500)));
+        
+        Mono<String> resp = webClient.post()
+                .uri("http://localhost/delay/6")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(Mono.just(book), Book.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(5))
+                .retryWhen(retry)  // <-- retry;
+                ;
+//        resp.log().subscribe();
+//        try {
+//			System.in.read();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+
+        System.out.println("result:{}" + resp.block());
+    }
+	
+	public void testFormParam(){
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("name1","value1");
+        formData.add("name2","value2");
+        Mono<String> resp = WebClient.create().post()
+                .uri("http://localhost/anything")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve().bodyToMono(String.class);
+        System.out.println("result:" + resp.block());
     }
 	
 //	public <T> void post(T input) {
